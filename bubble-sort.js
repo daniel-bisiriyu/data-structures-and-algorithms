cb730290895cdbdb431f1e772f6acbf9dc914a8d
daniel bisiriyu <daniel.bisiriyu.o@gmail.com>
2024-10-26 23:59:33 +0100

    bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,8 +1,11 @@
 const bubbleSort = (arr) => {
     const arrLength = arr.length;
+    let isSwapped = false
     for (i =0; i<arrLength;i++) {
+        
         for (j=0; j<arrLength-i+1; j++) {
             if (arr[i] > arr[i+1]) { 
+                isSwapped = true
                const larger = arr[i]
                arr[i] = arr[i+1]          
                arr[i+1] = larger;
