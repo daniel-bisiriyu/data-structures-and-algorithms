da849271acd404fe5175c1bee04c7a4f07a56294
daniel bisiriyu <daniel.bisiriyu.o@gmail.com>
2024-10-29 22:03:14 +0100

    Removed log statements and initialized selection sort file

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,10 +1,8 @@
 const bubbleSort = (arr) => {
     const arrLength = arr.length;
     for (i =0; i<arrLength; i++) {
-        console.log("outer-loop", i ,arr[i])
         let isSwapped = false;
         for (j=0; j<arrLength-i+1; j++) {
-            console.log("inner-loop",j ,arr[j])
             if (arr[j] > arr[j+1]) { 
                const larger = arr[j];
                arr[j] = arr[j+1];          
